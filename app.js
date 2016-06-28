7b0450c1b342355f9096954ef83bc8c4e247e183
Anton Reytarovskiy <Reitarovskiy.toh@gmail.com>
2016-06-29 01:19:22 +0300

    add service

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var phoneBook = angular.module('phone-book',[]);
 
 phoneBook.controller('phoneBookCtrl',function ($scope,persons) {
-    $scope.persons = persons;
+    $scope.persons = persons.list();
 });
 
 phoneBook.directive('pbPerson',function () {
@@ -14,55 +14,62 @@ phoneBook.directive('pbPerson',function () {
     }
 });
 
-phoneBook.factory('persons',function () {
-    return [
-        {
-            name: 'dasha',
-            phone: '+3809647625',
-            photo: 'dasha.jpg',
-            city: 'Zhmerynka'
-        },
-        {
-            name: 'nazar',
-            phone: '+3807584259',
-            photo: 'nazar.jpg',
-            city: 'Zhmerynka'
-        },
-        {
-            name: 'sergey',
-            phone: '+3872597258',
-            photo: 'sergey.jpg',
-            city: 'vinnytsia'
-        },
-        {
-            name: 'anton',
-            phone: '+3807891574',
-            photo: 'anton.jpg',
-            city: 'zhmerynka'
-        },
-        {
-            name: 'bill',
-            phone: '+78514597',
-            photo: 'bill.jpg',
-            city: 'vinnytsia'
-        },
-        {
-            name: 'iron-man',
-            phone: '999-443-323',
-            photo: 'iron-man.jpg',
-            city: 'london'
-        },
-        {
-            name: 'thor',
-            phone: '0800 3232 323',
-            photo: 'thor.jpg',
-            city: 'asgard'
-        },
-        {
-            name: 'gilfoyle',
-            phone: '555-555-555',
-            photo: 'gilfoyle.jpg',
-            city: 'london'
-        },
-    ]
+phoneBook.service('persons',function () {
+    function list() {
+        return [
+            {
+                name: 'dasha',
+                phone: '+3809647625',
+                photo: 'dasha.jpg',
+                city: 'Zhmerynka'
+            },
+            {
+                name: 'nazar',
+                phone: '+3807584259',
+                photo: 'nazar.jpg',
+                city: 'Zhmerynka'
+            },
+            {
+                name: 'sergey',
+                phone: '+3872597258',
+                photo: 'sergey.jpg',
+                city: 'vinnytsia'
+            },
+            {
+                name: 'anton',
+                phone: '+3807891574',
+                photo: 'anton.jpg',
+                city: 'zhmerynka'
+            },
+            {
+                name: 'bill',
+                phone: '+78514597',
+                photo: 'bill.jpg',
+                city: 'vinnytsia'
+            },
+            {
+                name: 'iron-man',
+                phone: '999-443-323',
+                photo: 'iron-man.jpg',
+                city: 'london'
+            },
+            {
+                name: 'thor',
+                phone: '0800 3232 323',
+                photo: 'thor.jpg',
+                city: 'asgard'
+            },
+            {
+                name: 'gilfoyle',
+                phone: '555-555-555',
+                photo: 'gilfoyle.jpg',
+                city: 'london'
+            },
+        ]
+    }
+
+    return {
+        list: list
+    }
 });
+
